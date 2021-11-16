4c406c2193a09ee6b1ad9feb5054568bfd31584a
Vladimir Zivkovic <vladax@gmail.com>
2021-11-16 19:18:55 +0100

    flash

--- hello.py
+++ hello.py
@@ -1,8 +1,16 @@
-from flask import Flask, render_template
-
+from flask import Flask, render_template, flash
+from flask_wtf import FlaskForm
+from wtforms import StringField, SubmitField
+from wtforms.validators import DataRequired
 
 #create a Flask Instance
 app = Flask(__name__)
+app.config['SECRET_KEY'] = "123321"
+
+# Create a form class
+class NamerForm(FlaskForm):
+    name = StringField("What is your name", validators=[DataRequired()])
+    submit = SubmitField("Stisni")
 
 #create a route decorator
 @app.route('/')
@@ -19,4 +27,16 @@ def index():
 def user(name):
     return render_template("user.html", user_name=name)
 
+
+#Create NAme PAge
+
+@app.route('/name', methods=['GET', 'POST'])
+def name():
+    name = None
+    form = NamerForm()
+    if form.validate_on_submit():
+        name = form.name.data
+        form.name.data = ''
+        flash("Uspesno prihvaceno")
+    return render_template("name.html", name = name, form = form)
 
